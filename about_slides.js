32a2e3da06991de81bb03b868a25600fa44dacbe
MateusPAbreu <122947765+MateusPAbreu@users.noreply.github.com>
2025-08-19 18:42:30 -0300

    Add files via upload

--- about_slides.js
+++ about_slides.js
@@ -11,13 +11,26 @@ let currentSlide = 0;
 
 function back(){
     // print("Its calling");
-    slides[currentSlide].style.display = "none";
+    slides[currentSlide].style.visibility = "hidden";
 
     if(currentSlide === 0 ){
         currentSlide = 5;
     }else{
         currentSlide = currentSlide-1;
     }
+    console.log(currentSlide)
+    slides[currentSlide].style.visibility = "visible";
+}
 
-    slides[currentSlide].style.display = "display";
+function forward(){
+    // print("Its calling");
+    slides[currentSlide].style.visibility = "hidden";
+
+    if(currentSlide === 5 ){
+        currentSlide = 0;
+    }else{
+        currentSlide = currentSlide+1;
+    }
+    console.log(currentSlide)
+    slides[currentSlide].style.visibility = "visible";
 }
